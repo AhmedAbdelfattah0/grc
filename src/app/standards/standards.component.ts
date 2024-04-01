3eade70bf86f250b1c0c4da39e55f0c36d147aec
Ahmed Abdelfatah <ahmed.m.abdelfttah@gmail.com>
2024-04-02 00:53:59 +0200

    feat: Add confirmation dialog and Arabic localization
    Introduce a new confirmation dialog for file deletion and
    incorporate Arabic translations for sidebar navigation items
    and standards component. Includes setup for dialog functionality
    and dynamic Arabic content based on user interaction. Enhances
    user experience and supports multilingual accessibility.

--- src/app/standards/standards.component.ts
+++ src/app/standards/standards.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 @Component({
   selector: 'app-standards',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule],
   templateUrl: './standards.component.html',
   styleUrl: './standards.component.scss'
 })
 export class StandardsComponent {
+  status1: string = ""
+  status2: string = ""
+  openToWork: boolean = false;
+  constructor(public dialog: MatDialog) {}
 
+  save() {
+    this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px'})
+  }
 }
